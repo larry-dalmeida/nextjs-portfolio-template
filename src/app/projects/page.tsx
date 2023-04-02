17112aba8468e31e14b03b278f5480d49d00841b
Larry D Almeida <hello@larrydalmeida.com>
2023-04-02 18:55:02 +0200

    First Release Ready

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+const Projects = () => (
+    <div>
+        <h1>Projects</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem dicta reiciendis nulla magnam! Adipisci placeat vero illum temporibus perferendis odio. Excepturi fugit vitae accusantium, laboriosam est numquam corrupti rerum!</p>
+    </div>
+);
+
+export default Projects;
